7a6536d463c6377968f260255694d123c8da8c93
svitorz <vitor.fabio.castro@gmail.com>
2023-10-05 21:03:23 -0300

    05.10
    Adicionando pesquisa no campo de produtos

--- main/listagem-produto.php
+++ main/listagem-produto.php
@@ -2,71 +2,246 @@
 session_start();
 require 'autenticacao.php';
 
-
 $titulo_pagina = "Listagem dos itens inseridos";
 
 require_once 'header.php';
 
 require 'conexao.php';
 
-$sql = "SELECT id,nome,urlfoto,descricao FROM produtos ORDER BY nome";
-$stmt = $conn->query($sql);
+if (isset($_GET["ordem"]) && !empty($_GET["ordem"])) {
+  $ordem = filter_input(INPUT_GET, "ordem", FILTER_SANITIZE_SPECIAL_CHARS);
+} else {
+  $ordem = "nome";
+}
+
+// if (isset($busca) && !empty($busca)) {
+// $tipo_busca = filter_input(INPUT_POST, "tipo_busca", FILTER_SANITIZE_SPECIAL_CHARS);
+// $busca = filter_input(INPUT_POST, "busca", FILTER_SANITIZE_SPECIAL_CHARS);
+
+//   if ($tipo_busca == "id") {
+
+//     $sql = "SELECT id,nome,urlfoto,descricao FROM produtos WHERE id = ? ORDER BY {$ordem}";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute([$busca]);
+
+//   } elseif ($tipo_busca == "nome") {
+
+//     $sql = "SELECT id,nome,urlfoto,descricao FROM produtos WHERE nome ilike ? ORDER BY {$ordem}";
+//     $busca_banco = "%" . $busca . "%";
+//     $busca_banco = str_replace(" ", "%", $busca_banco);
+
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute([$busca_banco]);
+
+//   } elseif ($tipo_busca == "descr") {
+
+//     $sql = "SELECT id,nome,urlfoto,descricao FROM produtos WHERE descricao ilike ? ORDER BY {$ordem}";
+//     $busca_banco = "%" . $busca . "%";
+//     $busca_banco = str_replace(" ", "%", $busca_banco);
+
+
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute([$busca_banco]);
+
+//   } else {
+
+//     $sql = "SELECT id,nome,urlfoto,descricao FROM produtos 
+//     WHERE descricao ilike ? 
+//       OR nome ilike ? 
+//         OR id = ? 
+//           ORDER BY {$ordem}";
+
+//     $buscaInt = intval($busca);
+//     $busca_banco = "%" . $busca . "%";
+//     $busca_banco = str_replace(" ", "%", $busca_banco);
+
+//     $stmt = $conn->prepare($sql);
+//     $stmt->execute([$busca_banco, $busca_banco, $buscaInt]);
+
+//   }
+
+// } else {
+//   $tipo_busca = null;
+//   $busca = null;
+
+//   $sql = "SELECT id,nome,urlfoto,descricao FROM produtos  ORDER BY {$ordem}";
+//   $stmt = $conn->query($sql);
+
+// }
+//Tentando refazer a pesquisa utilizando switch/case
+
+if (isset($busca) && !empty($busca)) {
+  $tipo_busca = filter_input(INPUT_POST, "tipo_busca", FILTER_SANITIZE_SPECIAL_CHARS);
+  $busca = filter_input(INPUT_POST, "busca", FILTER_SANITIZE_SPECIAL_CHARS);
+
+  $where = null;
+  switch ($tipo_busca) {
+    case "id":
+      $where = "WHERE id = ?";
+    case "nome":
+      $where = "WHERE nome ilike ?";
+    case "descr":
+      $where = "WHERE descricao ilike ?";
+      break;
+    default:
+      $where = "WHERE descricao ilike ? OR nome ilike ? OR id = ?";
+  }
+  if (isset($where)) {
+    $sql = "SELECT id,nome,urlfoto,descricao FROM produtos {$where} ORDER BY {$ordem}";
+    $busca_banco = "%" . $busca . "%";
+    $busca_banco = str_replace(" ", "%", $busca_banco);
+    $buscaInt = intval($busca);
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$busca_banco, $busca_banco, $buscaInt]);
+  } 
+} else {
+  $tipo_busca = null;
+  $busca = null;
+  
+  $sql = "SELECT id,nome,urlfoto,descricao FROM produtos  ORDER BY {$ordem}";
+  $stmt = $conn->query($sql);
+}
+
+?>
+<div class="row">
+  <div class="col">
+    <form action="?ordem=<?= $ordem ?>" role="search" method="POST" class="row">
+
+      <label for="tipo_busca" class="col-2">
+        Buscar por
+      </label>
+      <div class="col-sm-2">
+        <select class="form-select" name="tipo_busca" id="tipo_busca">
+          <option value="">Todos os campos</option>
+          <option value="id" <?php if ($tipo_busca == "id")
+            echo "selected"; ?>>Id</option>
+          <option value="nome" <?php if ($tipo_busca == "nome")
+            echo "selected"; ?>>Nome</option>
+          <option value="descr" <?php if ($tipo_busca == "descr")
+            echo "selected"; ?>>Descrição</option>
+        </select>
+      </div>
+      <div class="col-sm-6">
+        <input class="form-control me-2" type="search" id="busca" name="busca" placeholder="Search" aria-label="Search"
+          value="<?php if(isset($busca)){ echo $busca; } else {echo ""; }   ?>" />
+      </div>
+      <div class="col-sm-2">
+        <button type="submit" class="btn btn-outline-success">
+          Pesquisar
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<hr />
+
+<?php
+if (!empty($busca)) {
+  ?>
+  <div class="row">
+    <div class="row">
+      <div class="alert alert-light" role="alert">
+        <h4>Você está buscando por "<mark class="fst-italic">
+            <?= $busca; ?>
+          </mark>" ,<a href="?ordem=<?= $ordem ?>">Limpar</a> </h4>
+      </div>
+    </div>
+  </div>
+  <?php
+}
 ?>
 
-<table class="table table-striped table-hover">
-    <thead>
-        <tr>
-          <th scope="col" style="width: 5%;">ID</th>
-          <th scope="col" style="width: 15%;">Nome do Produto</th>
-          <th scope="col" style="width: 25%;">Descrição</th>
-          <th scope="col" style="width: 15%;">Link da imagem do produto</th>
-          <?php
-          if(autenticado()){
-          ?> 
-          <th scope="col" style="width:20%;"></th><th scope="col" style="width:20%;"></th>
-          <?php 
-          }
-          ?>
-        </tr>
-    </thead>
-    <tbody>
-      <?php 
-      while($row = $stmt->fetch()){
-      ?>
-        <tr>
-          <th scope="row"> <?= $row["id"]?> </th>
-          <td><?= $row["nome"]?></td>
-          <td><?= $row["descricao"]?></td>
-          <td>
-            <a href="<?= $row["urlfoto"]?>">
-              LINK
-            </a>
-          </td>
-          <td>
-            <?php 
-            if(autenticado()){
 
-             
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <?php
+      if ($ordem == "nome") {
+        ?>
+        <th scope="col" style="width: 5%;">
+          <a href="?ordem=id">
+            ID
+          </a>
+        </th>
+        <th scope="col" style="width: 15%;">
+          Nome do Produto
+          <span data-feather="chevron-down"></span>
+        </th>
+        <?php
+      } else {
+        ?>
+        <th scope="col" style="width: 5%;">
+          ID
+          <span data-feather="chevron-down"></span>
+        </th>
+        <th scope="col" style="width: 15%;">
+          <a href="?ordem=nome">
+            Nome do Produto
+          </a>
+        </th>
+        <?php
+      }
+      ?>
+
+
+      <th scope="col" style="width: 25%;">Descrição</th>
+      <th scope="col" style="width: 15%;">Link da imagem do produto</th>
+      <?php
+      if (autenticado()) {
+        ?>
+        <th scope="col" style="width:20%;"></th>
+        <th scope="col" style="width:20%;"></th>
+        <?php
+      }
+      ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    while ($row = $stmt->fetch()) {
+      ?>
+      <tr>
+        <th scope="row">
+          <?= $row["id"] ?>
+        </th>
+        <td>
+          <?= $row["nome"] ?>
+        </td>
+        <td>
+          <?= $row["descricao"] ?>
+        </td>
+        <td>
+          <a href="<?= $row["urlfoto"] ?>">
+            LINK
+          </a>
+        </td>
+        <td>
+          <?php
+          if (autenticado()) {
+
+
             ?>
-            <a href="excluir-produto.php?id=<?=$row['id'];?>" onclick="if(!confirm('Deseja excluir?')) return false;" class=" btn btn-sm btn-danger">
+            <a href="excluir-produto.php?id=<?= $row['id']; ?>" onclick="if(!confirm('Deseja excluir?')) return false;"
+              class=" btn btn-sm btn-danger">
               <span data-feather="trash-2"></span>
-                Excluir 
+              Excluir
             </a>
           </td>
           <td>
-            <a href="editar-produto.php?id=<?=$row['id'];?>" class="btn btn-sm btn-primary">
-                <span data-feather="edit"></span>
-                Editar
+            <a href="editar-produto.php?id=<?= $row['id']; ?>" class="btn btn-sm btn-primary">
+              <span data-feather="edit"></span>
+              Editar
             </a>
           </td>
         </tr>
-        <?php 
-        }
-        }
-        ?>
-    </tbody>
+        <?php
+          }
+    }
+    ?>
+  </tbody>
 </table>
 
 <?php
 require_once 'footer.php';
-?>            
+?>
--- main/sql/tabela produtos_POSTGRESQL.sql
+++ main/sql/tabela produtos_POSTGRESQL.sql
@@ -15,4 +15,41 @@ INSERT INTO produtos(nome, urlfoto, descricao)
     VALUES ('Chinelo','https://havaianas.com.br/dw/image/v2/BDDJ_PRD/on/demandware.static/-/Sites-havaianas-master/default/dw978a6e76/product-images/4001280_0031_HAVAIANAS%20TRADICIONAL_C.png','Este é o modelo que deu início à história de Havaianas e traduz a autenticidade da marca: para alguns é uma Havaianas com preço acessível; para muitos, representa um produto vintage que traz boas lembranças.');
 
 INSERT INTO produtos(nome, urlfoto, descricao) 
-    VALUES ('Carregador De Celular','https://imgs.ponto.com.br/1511673500/2xg.jpg','Os caregadores Turbo utilizam a tecnologia Quick Charge 2.0. Eles são capazes de carregar o seu aparelho até 4 vezes mais rápido. Esses carregadores trabalham com tensões e intensidades de corrente mais elevadas, levando mais energia ao mesmo tempo para a bateria do seu aparelho');    
+    VALUES ('Carregador de Celular','https://imgs.ponto.com.br/1511673500/2xg.jpg','Os caregadores Turbo utilizam a tecnologia Quick Charge. Eles são capazes de carregar o seu aparelho até 4 vezes mais rápido. Esses carregadores trabalham com tensões e intensidades de corrente mais elevadas, levando mais energia ao mesmo tempo para a bateria do seu aparelho');    
+
+INSERT INTO produtos(nome, urlfoto, descricao) 
+    VALUES ('Almofada de cachorro','https://ae01.alicdn.com/kf/Sfedc4518d6514fa9832df0aa813ddab5q/Travesseiro-decorativo-em-forma-de-cachorro-3d-almofada-de-lance-com-algod-o-pp-interno-decora.jpg','Enchimento:	Algodão
+Cor:	Multicolorido
+Material:	Algodão
+Composição:	100% Algodão
+Não veja esse vídeo: http://bit.ly/3REDEW7');    
+
+INSERT INTO produtos(nome, urlfoto, descricao) 
+    VALUES ('Homem aranha da shopee','https://down-br.img.susercontent.com/file/br-11134207-23020-wzsw3fx0mzmv63','Boneco do Homem Aranha Miniatura em pelúcia velboa importada
+
+Perfeito para quem carrega o seu super heróis no peito com muito orgulho. Um bonequinho bonito que seus filhos vão adorar. O brinquedo também é um presente perfeito para todos os fãs do Homem-Aranha.
+
+Um bonequinho lindo e super legal do homem aranha. Ele é fofinho e apalpável com composição em 100% pelúcia velboa
+Especificações:
+Indicado para crianças acima de 12 meses');  
+
+INSERT INTO produtos(nome, urlfoto, descricao) 
+    VALUES ('Tênis de malha reciclado masculino preto','https://balenciaga.dam.kering.com/m/537620fbfd513896/Large-758483W2DG11090_F.jpg','• Sneaker
+• Technical 3D knit
+• Balenciaga logo at the edge of the toe and at back
+• Contrasted logo printed on exterior
+• Ribbed finishing
+• Made in Italy
+• Do not machine wash and wipe with a soft cloth
+Material: 92% polyester, 8% elastane
+Product ID: 758483W2DG11090');  
+
+INSERT INTO produtos(nome, urlfoto, descricao) 
+    VALUES ('Jogo de cartas Uno minimalista, Mattel, preto','https://m.media-amazon.com/images/I/61mnv5uW8GL._AC_SL1500_.jpg','Edição especial - visual completamente único
+Minimalista - projetado com uma estética minimalista, esta versão é um ótimo presente para os colecionadores uno
+Visual novo - visual totalmente novo, bonito e simplista idealizado pelo designer warleson oliveira
+Jogo - o jogo é como o uno clássico, de 2 a 10 jogadores com 7 anos ou mais');  
+
+
+    
+
